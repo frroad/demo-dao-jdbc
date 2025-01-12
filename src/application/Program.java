865b45b77fcffe86e8baffdc574763f1fe09114e
frroad <frramos@outlook.com.br>
2025-01-12 20:52:18 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,15 @@
 package application;
 
+
+
+
+
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +18,9 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
-	}
-
+		
+		Seller seller = new Seller(21,"Ana","Anc@gmail.com",new Date(),2000.0,obj);
+		
+		System.out.println(seller);
+}
 }
